978a4ba41e1249f3cb91fee01ea117a9a158e1ac
MarieMittu <lady.marvis@icloud.com>
2022-02-22 21:30:27 +0100

    create project

--- Edutainment/StartScreen.swift
+++ Edutainment/StartScreen.swift
@@ -12,7 +12,7 @@ struct StartScreen: View {
     
     var body: some View {
         VStack {
-            Text("Choose the level")
+            Text("Choose the level:")
             Button("Easy") {numberOfQuestions = 3}
             Button("Medium") {}
             Button("Hard") {}
